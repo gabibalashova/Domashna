282c0b649b3985c715f86fda7e2b4bc195d15be3
Gabriela Balashova <gabrielabalashova@gmail.com>
2026-03-08 18:21:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the 1st word in separate letters: ");
+            string[] arr1 = Console.ReadLine().Split(' ').ToArray();
+
+            Console.Write("Enter the 2nd word in separate letters: ");
+            string[] arr2 = Console.ReadLine().Split(' ').ToArray();
+
+            int minLength = Math.Min(arr1.Length, arr2.Length);
+            bool arr1First = false;
+            int i;
+            for (i = 0; i < minLength; i++)
+            {
+                if (string.Compare(arr1[i], arr2[i]) < 0)
+                {
+                    arr1First = true;
+                    break;
+                }
+                else if (string.Compare(arr1[i], arr2[i]) > 0)
+                {
+                    arr1First = false;
+                    break;
+                }
+            }
+
+            if (arr1First || (arr1.Length < arr2.Length && minLength == arr1.Length))
+            {
+                Console.WriteLine(arr1[i]);
+                Console.WriteLine(arr2[i]);
+            }
+            else
+            {
+                Console.WriteLine(arr2[i]);
+                Console.WriteLine(arr1[i]);
+            }
+        }
+    }
+}
